447b5c7384fe4c2368cb15abeb942007b9df9766
Nhat Le <nhat.n.le@sjsu.edu>
2022-01-17 23:10:50 -0800

    commit

---
+++ build/test.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = __importDefault(require("lodash"));
+console.log(lodash_1.default.add(3, 4));
+console.log(lodash_1.default.multiply(3, 4));
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+import _ from 'lodash';
+
+console.log(_.add(3, 4));
+console.log(_.multiply(3, 4));
